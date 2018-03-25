4ca9f8d86e883a674b5501a08374efae78c97730
divyanshu9606 <38567602+divyanshu9606@users.noreply.github.com>
2018-03-25 22:49:41 +0530

    Com1

---
+++ PR1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+ int main()
+{
+    int gt[20],pn[20],wt[20],varib[20],nom[20],i,j,n,tot=0,postn,varib1,mean_wt,mean_varib,varib2;
+    printf("<-----------------------Enter total Number of Persons:-------------------Max(10)>");
+    scanf("%d",&n);
+}
